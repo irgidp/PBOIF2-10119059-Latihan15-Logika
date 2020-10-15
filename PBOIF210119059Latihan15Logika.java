f9fc9cd5576da0c248b1f7dcf05779b4591fc29a
irgidp <irgijak@gmail.com>
2020-10-15 22:28:47 +0700

    Latihan 15

---
+++ PBOIF210119059Latihan15Logika.java
@@ -0,0 +1,22 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan logika(2)
+ */
+package pboif2.pkg10119059.latihan15.logika;
+
+public class PBOIF210119059Latihan15Logika {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a );
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
